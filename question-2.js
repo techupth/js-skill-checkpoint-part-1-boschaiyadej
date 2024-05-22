0ce7b3f8dbf5415a8718ceae2ac8018b01130004
bosch <130572668+boschaiyadej@users.noreply.github.com>
2024-05-22 15:17:01 +0700

    do skill checkpoints

--- question-2.js
+++ question-2.js
@@ -4,3 +4,17 @@ const inventory = [
   { name: "Banana", price: 10, quantity: 50 },
 ];
 // เริ่มเขียนโค้ดตรงนี้
+// 1) ให้แก้ไขจำนวนสินค้า “Apple” จาก 100 เป็น 200
+inventory[0].quantity = 200;
+console.log(inventory);
+
+// 2) เพิ่มสินค้าใหม่ที่ชื่อ “Orange” ที่มีราคา 20 บาท และมีจำนวน 300 ชิ้นในสต็อก
+inventory.push({ name: "Orange", price: 20, quantity: 300 });
+console.log(inventory);
+
+// 3) ให้คำนวณมูลค่ารวมของจำนวนสินค้าทั้งหมดในสต็อก ผลลัพธ์ของโปรแกรมควรจะแสดงออกมาทางหน้าจอ Console แบบนี้ มูลค่ารวมของจำนวนสินค้าทั้งหมดในสต็อก 13500 บาท
+let totalValue = 0;
+for (let index = 0; index < inventory.length; index++) {
+  totalValue += inventory[index].price * inventory[index].quantity;
+}
+console.log(`มูลค่ารวมของจำนวนสินค้าทั้งหมดในสต็อก ${totalValue} บาท`);
